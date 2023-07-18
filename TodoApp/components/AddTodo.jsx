037772134c237372e778ab88bad9388d7b301502
juseong <jjs7280@gmail.com>
2023-07-18 15:35:30 +0900

    3.2 TextInput으로 사용자 키보드 입력받기

--- TodoApp/components/AddTodo.jsx
+++ TodoApp/components/AddTodo.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Image,
+  Keyboard,
+  StyleSheet,
+  TextInput,
+  TouchableNativeFeedback,
+  View,
+} from 'react-native';
 
 export default function AddTodo() {
-  return <View style={styles.block} />;
+  const [text, setText] = useState('');
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할일을 입력하세요."
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={onPress}
+      />
+      <View style={styles.circleWrapper}>
+        <TouchableNativeFeedback>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
   },
 });
