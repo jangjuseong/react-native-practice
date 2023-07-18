3ea2dc18eaf822108a04a6da3da3efc0addbbc13
juseong <jjs7280@gmail.com>
2023-07-18 16:58:48 +0900

    4.2

--- TodoApp/App.jsx
+++ TodoApp/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 export default function App() {
   const today = new Date();
 
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
+
   return (
     <SafeAreaView edges={['bottom']} style={styles.block}>
       <DateHead date={today} />
-      <Empty />
+      {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
+
       <AddTodo />
     </SafeAreaView>
   );
