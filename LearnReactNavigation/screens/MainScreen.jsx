7d4c750d4a8166ed9fdd76294ccec7b1c571e394
juseong <jjs7280@gmail.com>
2023-07-19 17:44:51 +0900

    5.3.2 하단 탭 네비게이터

---
+++ LearnReactNavigation/screens/MainScreen.jsx
@@ -0,0 +1,100 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Button
+        title="Detail 1 열기"
+        onPress={() => navigation.push('Detail', {id: 1})}
+      />
+    </View>
+  );
+}
+
+function SearchScreen() {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+}
+
+function NotificationScreen() {
+  return (
+    <View>
+      <Text>Notification</Text>
+    </View>
+  );
+}
+
+function MessageScreen() {
+  return (
+    <View>
+      <Text>Message</Text>
+    </View>
+  );
+}
+
+const MainScreen = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: '#fb8c00',
+        tabBarShowLabel: false,
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: '홈',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          title: '검색',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          title: '알림',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color, size}) => (
+            <Icon name="notifications" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Message"
+        component={MessageScreen}
+        options={{
+          title: '메시지',
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({color, size}) => (
+            <Icon name="message" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default MainScreen;
