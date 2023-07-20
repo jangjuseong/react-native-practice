14696abb7082553e79dee0fd5cbe42030e2408da
juseong <jjs7280@gmail.com>
2023-07-20 15:05:03 +0900

    5.4 내비게이션 Hooks

--- LearnReactNavigation/screens/DetailScreen.jsx
+++ LearnReactNavigation/screens/DetailScreen.jsx
@@ -1,5 +1,11 @@
+import {useRoute} from '@react-navigation/native';
 import React, {useEffect} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
+
+function IDText() {
+  const route = useRoute();
+  return <Text style={styles.text}>id: {route.params.id}</Text>;
+}
 
 const DetailScreen = ({route, navigation}) => {
   useEffect(() => {
@@ -9,7 +15,7 @@ const DetailScreen = ({route, navigation}) => {
   }, [navigation, route.params.id]);
   return (
     <View style={styles.block}>
-      <Text style={styles.text}>id: {route.params.id}</Text>
+      <IDText />
       <View style={styles.buttons}>
         <Button
           title="다음"
--- LearnReactNavigation/screens/HomeScreen.jsx
+++ LearnReactNavigation/screens/HomeScreen.jsx
@@ -1,25 +1,23 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Button, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 
-const HomeScreen = ({navigation}) => {
+function OpenDetailButton() {
+  const navigation = useNavigation();
+
+  return (
+    <Button
+      title="Detail 1 열기"
+      onPress={() => navigation.push('Detail', {id: 1})}
+    />
+  );
+}
+
+const HomeScreen = () => {
   return (
     <View>
-      <Button
-        title="Detail 1 열기"
-        onPress={() => navigation.push('Detail', {id: 1})}
-      />
-      <Button
-        title="Detail 2 열기"
-        onPress={() => navigation.push('Detail', {id: 2})}
-      />
-      <Button
-        title="Detail 3 열기"
-        onPress={() => navigation.push('Detail', {id: 3})}
-      />
-      <Button
-        title="Headerless 열기"
-        onPress={() => navigation.push('Headerless')}
-      />
+      <Text>Home</Text>
+      <OpenDetailButton />
     </View>
   );
 };
